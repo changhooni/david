274ab510894cc7f7cea7635b7cb88f068edcb796
changhooni <reviewsize@gmail.com>
2025-08-07 16:00:18 +0900

    Feat: 메뉴 화면추가
    - /menu 라우트 추가
    - 메뉴 페이지에 버튼 3개 추가
    
    Resolves: #1

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ def hello_world():
     return "Hello, DevOps!"
 
 #route menu 추가
-#@app.route("/meun")
-#def meun():
-#    return render_template("menu.html")
+@app.route("/meun")
+def meun():
+    return render_template("menu.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
