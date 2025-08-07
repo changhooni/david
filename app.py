5823c52e5878343b9fe8aefab3ce6ab8d7eca4ce
changhooni <reviewsize@gmail.com>
2025-08-07 15:11:54 +0900

    Retry Git file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+# pip install flask 설치 명령어
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello, DevOps!"
+
+#route menu 추가
+#@app.route("/meun")
+#def meun():
+#    return render_template("menu.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
