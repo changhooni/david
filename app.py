1db91ddbd16d497b500231904ab6808bf7a7207b
changhooni <reviewsize@gmail.com>
2025-08-08 12:09:01 +0900

    hostname socket modify app.py new file index.html commit

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import socket
 # pip install flask 설치 명령어
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, DevOps!"
+def home():
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+
+    return render_template('index.html', computername=hostname)
+
+#def hello_world():
+#    return "Hello, DevOps!"
 
 #route menu 추가
 @app.route("/menu")
@@ -12,4 +21,5 @@ def meun():
     return render_template("menu.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    print(f"__debug__ is {__debug__}")
