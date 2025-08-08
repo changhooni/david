672d6fb61d02f5e49fd367a9838ce12b13eedb91
Rookie_codyssey <reviewsize@naver.com>
2025-08-08 22:51:30 +0900

    modify pccp_test_1.py commit

--- pccp_test_1.py
+++ pccp_test_1.py
@@ -4,7 +4,7 @@ def solutions(nums):
     p = []
 
     for i in nums:
-        if i not in p:
+        if i not in p: #중복 처리
             p.append(i)
     
     if len(nums) < max_su:
@@ -16,8 +16,8 @@ def solutions(nums):
 
 def solution(nums):
     answer = 0
-    max_su = len(nums) // 2
-    p = set(nums)
+    max_su = len(nums) // 2 # 리스트에 반으로 나누는 최대치
+    p = set(nums) # set 내장 함수 중복처리
     answer = min(len(p), max_su)
 
     return answer
