56ced85954a1b449760641b7b24f3df0028b0e71
changhooni <reviewsize@gmail.com>
2025-08-18 14:07:41 +0900

    modify file app.py

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ def home():
 def meun():
     return render_template("menu.html")
 
+#route test1 추가
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
     print(f"__debug__ is {__debug__}")
